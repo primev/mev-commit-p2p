6cc5c3b08ac47bcd40057cd1e55d77bdfe565a86
kant <kant@saga.xyz>
2023-10-05 17:35:58 -0700

    modifying entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,23 +6,26 @@ echo "Node Type: $NODE_TYPE"
 KEY_PATH="/keys"
 CONFIG_PATH="/config"
 
-# Generate the private key based on node type
+# Check whether the private key file based on node type already exists
 if [ "$NODE_TYPE" = "bootnode" ]; then
-    /app/mev-commit create-key ${KEY_PATH}/bootnode
     PRIV_KEY_FILE="${KEY_PATH}/bootnode"
     CONFIG_FILE="${CONFIG_PATH}/bootnode.yml"
 elif [ "$NODE_TYPE" = "builder" ]; then
-    /app/mev-commit create-key ${KEY_PATH}/builder
     PRIV_KEY_FILE="${KEY_PATH}/builder"
     CONFIG_FILE="${CONFIG_PATH}/builder.yml"
 else
-    /app/mev-commit create-key ${KEY_PATH}/searcher
     PRIV_KEY_FILE="${KEY_PATH}/searcher"
     CONFIG_FILE="${CONFIG_PATH}/searcher.yml"
 fi
 
-# Update the private key path in the configuration
-sed -i "s|priv_key_file:.*|priv_key_file: ${PRIV_KEY_FILE}|" ${CONFIG_FILE}
+# Generate the private key based on node type only if it doesn't exist yet
+if [ ! -f "$PRIV_KEY_FILE" ]; then
+    /app/mev-commit create-key ${PRIV_KEY_FILE}
+    # Update the private key path in the configuration
+    sed -i "s|priv_key_file:.*|priv_key_file: ${PRIV_KEY_FILE}|" ${CONFIG_FILE}
+else
+    echo "Key file ${PRIV_KEY_FILE} already exists. Skipping key creation."
+fi
 
 # If this is not the bootnode, update the bootnodes entry with P2P ID
 if [ "$NODE_TYPE" != "bootnode" ]; then
@@ -36,12 +39,14 @@ if [ "$NODE_TYPE" != "bootnode" ]; then
     echo "Response from bootnode:"
     echo "$BOOTNODE_RESPONSE"
 
-    if [ -n "$BOOTNODE_P2P_ID" ]; then
+    if [ -n "$BOOTNODE_P2P_ID" ] && [ ! $(grep -q "${BOOTNODE_P2P_ID}" "${CONFIG_FILE}") ]; then
         sed -i "s|<p2p_ID>|${BOOTNODE_P2P_ID}|" ${CONFIG_FILE}
-	sed -i "s|<localhost>|${BOOTNODE_IP}|" ${CONFIG_FILE}
-    else
+        sed -i "s|<localhost>|${BOOTNODE_IP}|" ${CONFIG_FILE}
+    elif [ -z "$BOOTNODE_P2P_ID" ]; then
         echo "Failed to fetch P2P ID from bootnode. Exiting."
         exit 1
+    else
+        echo "P2P ID is already configured. Skipping."
     fi
 fi
 
