22f15d8f642f27cda6f11d43e5a75c8860335765
Kartik Chopra <k5chopra@uwaterloo.ca>
2023-12-12 16:59:37 -0500

    Nit: Update git to https (#118)
    * uses http rather than password protected ssh connection to pull git repo.
    
    * Remove integretation-test changes for now.
    
    * update cli prompt.
    
    ---------
    
    Co-authored-by: Kartik Chopra <k5chopra@edu.uwaterloo.ca>

--- mev-commit-cli.sh
+++ mev-commit-cli.sh
@@ -36,9 +36,9 @@ create_primev_dir() {
 clone_repos() {
     echo "Cloning repositories under $PRIMEV_DIR..."
     # Clone only if the directory doesn't exist
-    [ ! -d "$GETH_POA_PATH" ] && git clone git@github.com:primevprotocol/go-ethereum.git "$GETH_POA_PATH"
-    [ ! -d "$CONTRACTS_PATH" ] && git clone git@github.com:primevprotocol/contracts.git "$CONTRACTS_PATH"
-    [ ! -d "$MEV_COMMIT_PATH" ] && git clone git@github.com:primevprotocol/mev-commit.git "$MEV_COMMIT_PATH"
+    [ ! -d "$GETH_POA_PATH" ] && git clone https://github.com/primevprotocol/go-ethereum.git "$GETH_POA_PATH"
+    [ ! -d "$CONTRACTS_PATH" ] && git clone https://github.com/primevprotocol/contracts.git "$CONTRACTS_PATH"
+    [ ! -d "$MEV_COMMIT_PATH" ] && git clone https://github.com/primevprotocol/mev-commit.git "$MEV_COMMIT_PATH"
 }
 
 # Function to pull latest changes for all repositories
@@ -53,7 +53,7 @@ update_repos() {
 start_settlement_layer() {
     local datadog_key=$1
 
-    git clone git@github.com:primevprotocol/go-ethereum.git "$GETH_POA_PATH"
+    git clone https://github.com/primevprotocol/go-ethereum.git "$GETH_POA_PATH"
     echo "Starting Settlement Layer..."
 
     cat > "$GETH_POA_PATH/geth-poa/.env" <<EOF
@@ -68,7 +68,7 @@ EOF
     make -C "$GETH_POA_PATH"/geth-poa up-prod-settlement
 }
 
-start_mev_commit() {
+ start_mev_commit() {
     local datadog_key=$1
     echo "Starting MEV-Commit..."
     DD_KEY="$datadog_key" docker compose -f "$MEV_COMMIT_PATH/integration-compose.yml" up --build -d
@@ -176,7 +176,7 @@ case "$1" in
         cleanup
         ;;
     *)
-        echo "Usage: $0 {start|update|cleanup} [rpc-url] [datadog-key]"
+        echo "Usage: $0 {start|update|cleanup|sl} [rpc-url] [datadog-key]"
         exit 1
 esac
 
