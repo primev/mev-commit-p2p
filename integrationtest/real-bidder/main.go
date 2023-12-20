87b2b16e5c46d8295deaff46daad9ae0464d09b2
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2023-12-20 01:47:12 -0500

    nit fixes and adds throtle.

--- integrationtest/real-bidder/main.go
+++ integrationtest/real-bidder/main.go
@@ -15,7 +15,7 @@ import (
 	"time"
 
 	"github.com/ethereum/go-ethereum/ethclient"
-	pb "github.com/primevprotocol/mev-commit/gen/go/rpc/userapi/v1"
+	pb "github.com/primevprotocol/mev-commit/gen/go/rpc/bidderapi/v1"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"google.golang.org/grpc"
@@ -41,20 +41,20 @@ var (
 var (
 	receivedPreconfs = prometheus.NewCounter(prometheus.CounterOpts{
 		Namespace: "mev_commit",
-		Subsystem: "user_emulator",
+		Subsystem: "bidder_emulator",
 		Name:      "total_received_preconfs",
 		Help:      "Total number of preconfs received from mev_commit nodes",
 	})
 	sentBids = prometheus.NewCounter(prometheus.CounterOpts{
 		Namespace: "mev_commit",
-		Subsystem: "user_emulator",
+		Subsystem: "bidder_emulator",
 		Name:      "total_sent_bids",
 		Help:      "Total number of bids sent to mev_commit nodes",
 	})
 	sendBidDuration = *prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Namespace: "mev_commit",
-			Subsystem: "user_emulator",
+			Subsystem: "bidder_emulator",
 			Name:      "send_bid_duration",
 			Help:      "Duration of method calls.",
 		},
@@ -104,8 +104,8 @@ func main() {
 	}
 	defer conn.Close()
 
-	userClient := pb.NewUserClient(conn)
-	err = checkOrStake(userClient, logger)
+	bidderClient := pb.NewBidderClient(conn)
+	err = checkOrStake(bidderClient, logger)
 	if err != nil {
 		logger.Error("failed to check or stake", "err", err)
 		return
@@ -113,62 +113,30 @@ func main() {
 
 	wg := sync.WaitGroup{}
 	wg.Add(1)
-	go func() {
+	go func(logger *slog.Logger) {
 		defer wg.Done()
-		bc := BlockCache{}
 		for {
-			block, blkNum, err := bc.RetreivedBlock(rpcClient)
+			block, blkNum, err := RetreivedBlock(rpcClient)
 			if err != nil {
 				logger.Error("failed to get block", "err", err)
 			} else {
+				throtle := time.Duration(12000*time.Millisecond) / time.Duration(len(block))
+				logger.Info("thortling set", "throtle", throtle.String())
 				for j := 0; j < len(block); j++ {
-					err = sendBid(userClient, logger, rpcClient, block[j], int64(blkNum))
+					err = sendBid(bidderClient, logger, rpcClient, block[j], int64(blkNum))
 					if err != nil {
 						logger.Error("failed to send bid", "err", err)
 					}
-					time.Sleep(1 * time.Second)
+					time.Sleep(throtle)
 				}
-				// for _, txn := range block {
-				// 	err = sendBid(userClient, logger, rpcClient, txn, int64(blkNum))
-				// 	if err != nil {
-				// 		logger.Error("failed to send bid", "err", err)
-				// 	}
-				// 	time.Sleep(50 * time.Millisecond)
-				// }
 			}
 		}
-	}()
+	}(logger)
 
 	wg.Wait()
 }
 
-type BlockCache struct {
-	lastRetreived time.Time
-	lastBlockTxns []string
-	blkNum        int64
-}
-
-func (b BlockCache) RetreivedBlock(rpcClient *ethclient.Client) ([]string, int64, error) {
-	// now := time.Now()
-	// if now.Sub(b.lastRetreived) < 13*time.Second {
-	// 	return b.lastBlockTxns, b.blkNum, nil
-	// 	// b.lastBlockTxns = []string{}
-	// 	// // Generate Random txnhashes
-	// 	// for i := 0; i < 100; i++ {
-	// 	// 	txHash := crypto.Keccak256Hash(
-	// 	// 		append(
-	// 	// 			[]byte(now.String()),
-	// 	// 			append(
-	// 	// 				[]byte(fmt.Sprintf("%d", i)),
-	// 	// 				[]byte(fmt.Sprintf("%d", b.blkNum))...,
-	// 	// 			)...,
-	// 	// 		),
-	// 	// 	)
-	// 	// 	b.lastBlockTxns = append(b.lastBlockTxns, txHash.Hex())
-	// 	// }
-	// 	// return b.lastBlockTxns, b.blkNum, nil
-	// }
-
+func RetreivedBlock(rpcClient *ethclient.Client) ([]string, int64, error) {
 	blkNum, err := rpcClient.BlockNumber(context.Background())
 	if err != nil {
 		return nil, -1, err
@@ -178,23 +146,20 @@ func (b BlockCache) RetreivedBlock(rpcClient *ethclient.Client) ([]string, int64
 		return nil, -1, err
 	}
 
-	lastBlockTxns := []string{}
+	blockTxns := []string{}
 	txns := fullBlock.Transactions()
 	for _, txn := range txns {
-		lastBlockTxns = append(lastBlockTxns, txn.Hash().Hex())
+		blockTxns = append(blockTxns, txn.Hash().Hex())
 	}
 
-	b.lastBlockTxns = lastBlockTxns
-	// b.lastRetreived = now
-
-	return b.lastBlockTxns, int64(blkNum), nil
+	return blockTxns, int64(blkNum), nil
 }
 
 func checkOrStake(
-	userClient pb.UserClient,
+	bidderClient pb.BidderClient,
 	logger *slog.Logger,
 ) error {
-	stakeAmt, err := userClient.GetStake(context.Background(), &pb.EmptyMessage{})
+	stakeAmt, err := bidderClient.GetStake(context.Background(), &pb.EmptyMessage{})
 	if err != nil {
 		logger.Error("failed to get stake amount", "err", err)
 		return err
@@ -209,11 +174,11 @@ func checkOrStake(
 	}
 
 	if stakedAmt.Cmp(big.NewInt(0)) > 0 {
-		logger.Error("user already staked")
+		logger.Error("bidder already staked")
 		return nil
 	}
 
-	_, err = userClient.RegisterStake(context.Background(), &pb.StakeRequest{
+	_, err = bidderClient.RegisterStake(context.Background(), &pb.StakeRequest{
 		Amount: "10000000000000000000",
 	})
 	if err != nil {
@@ -227,7 +192,7 @@ func checkOrStake(
 }
 
 func sendBid(
-	userClient pb.UserClient,
+	bidderClient pb.BidderClient,
 	logger *slog.Logger,
 	rpcClient *ethclient.Client,
 	txnHash string,
@@ -245,7 +210,7 @@ func sendBid(
 	logger.Info("sending bid", "bid", bid)
 
 	start := time.Now()
-	rcv, err := userClient.SendBid(context.Background(), bid)
+	rcv, err := bidderClient.SendBid(context.Background(), bid)
 	if err != nil {
 		logger.Error("failed to send bid", "err", err)
 		return err
