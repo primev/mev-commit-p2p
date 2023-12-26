961211e9bab335103b8ff2ccf3eb312b0e9bace4
alok <aloknerurkar@no-reply.com>
2023-12-27 04:33:32 +0530

    chore: remove offset

--- integrationtest/real-bidder/entrypoint.sh
+++ integrationtest/real-bidder/entrypoint.sh
@@ -3,4 +3,4 @@
 sleep 30
 
 echo "starting bidder-emulator with : ${BIDDER_IP} and rpc url ${L1_RPC_URL}"
-/app/bidder-emulator --server-addr ${BIDDER_IP} --rpc-addr ${L1_RPC_URL} --offset ${OFFSET}
+/app/bidder-emulator --server-addr ${BIDDER_IP} --rpc-addr ${L1_RPC_URL}
