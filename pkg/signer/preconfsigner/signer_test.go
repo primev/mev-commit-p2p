dca7a1bbe20794d3a53768c4b76f84de1aaf209e
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2024-03-02 13:44:09 -0500

    adds an additional test to ensure signatures are working correctly.

--- pkg/signer/preconfsigner/signer_test.go
+++ pkg/signer/preconfsigner/signer_test.go
@@ -145,6 +145,51 @@ func TestHashing(t *testing.T) {
 	})
 }
 
+func TestSignature(t *testing.T) {
+	t.Parallel()
+	// alice keys 0x328809Bc894f92807417D2dAD6b7C998c1aFdac6
+	pkey, err := crypto.HexToECDSA("9C0257114EB9399A2985F8E75DAD7600C5D89FE3824FFA99EC1C3EB8BF3B0501")
+	if err != nil {
+		t.Fatal(err)
+	}
+	keySigner := mockkeysigner.NewMockKeySigner(pkey, crypto.PubkeyToAddress(pkey.PublicKey))
+	bidder := preconfsigner.NewSigner(keySigner)
+
+	bid, err := bidder.ConstructSignedBid("0xkartik", big.NewInt(2), big.NewInt(2), 10, 20)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// bob keys 0x1D96F2f6BeF1202E4Ce1Ff6Dad0c2CB002861d3e
+	providerKey, err := crypto.HexToECDSA("38E47A7B719DCE63662AEAF43440326F551B8A7EE198CEE35CB5D517F2D296A2")
+	if err != nil {
+		t.Fatal(err)
+	}
+	keySigner = mockkeysigner.NewMockKeySigner(providerKey, crypto.PubkeyToAddress(providerKey.PublicKey))
+	provider := preconfsigner.NewSigner(keySigner)
+	preconf, err := provider.ConstructPreConfirmation(bid)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expBidDigest := "a0327970258c49b922969af74d60299a648c50f69a2d98d6ab43f32f64ac2100"
+	expBidSig := "876c1216c232828be9fabb14981c8788cebdf6ed66e563c4a2ccc82a577d052543207aeeb158a32d8977736797ae250c63ef69a82cd85b727da21e20d030fb311b"
+	expCommitmentDigest := "54c118e537dd7cf63b5388a5fc8322f0286a978265d0338b108a8ca9d155dccc"
+	expCommitmentSig := "ec0f11f77a9e96bb9c2345f031a5d12dca8d01de8a2e957cf635be14802f9ad01c6183688f0c2672639e90cc2dce0662d9bea3337306ca7d4b56dd80326aaa231b"
+	if hex.EncodeToString(preconf.Bid.Digest) != expBidDigest {
+		t.Fatalf("digest mismatch: %s != %s", hex.EncodeToString(preconf.Bid.Digest), expBidDigest)
+	}
+	if hex.EncodeToString(preconf.Bid.Signature) != expBidSig {
+		t.Fatalf("signature mismatch: %s != %s", hex.EncodeToString(preconf.Bid.Signature), expBidSig)
+	}
+	if hex.EncodeToString(preconf.Digest) != expCommitmentDigest {
+		t.Fatalf("digest mismatch: %s != %s", hex.EncodeToString(preconf.Digest), expCommitmentDigest)
+	}
+	if hex.EncodeToString(preconf.Signature) != expCommitmentSig {
+		t.Fatalf("signature mismatch: %s != %s", hex.EncodeToString(preconf.Signature), expCommitmentSig)
+	}
+}
+
 func TestVerify(t *testing.T) {
 	t.Parallel()
 
